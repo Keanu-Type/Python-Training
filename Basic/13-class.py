533ed08e00403c5c1660f6c01e8ca2479ad03b25
Keanu-Type <keanuberchestype0@gmail.com>
2022-12-02 22:15:42 +0800

    class

--- Basic/13-class.py
+++ Basic/13-class.py
@@ -13,13 +13,59 @@ point1.y = 20
 print(point1.x)
 print(point1.y)
 
-#CONSTRUCTOR
+#----------CONSTRUCTOR-------
 class Construct: #POINT
-    def __init__(self): #CONSTRUCTOR
-        pass
+    def __init__(self, x, y): #CUSTOM  MAKING CONSTRUCTOR
+        self.x = x
+        self.y = y 
     def move(self): #blueprint of POINT
         print("move!")
     
     def draw(self): #blueprint of POINT
         print("draw")
 
+
+point = Construct(10,20)
+#you dont need to put self.x = 10 and self.y manually
+print(point.x)
+print(point.y)
+
+#make a constructor with person:
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def talk(self):
+        print(f"Hello!{self.name}") #you can call other blueprint/constructor inside this classes
+
+user = Person("Keanu") #default, will be plased on def  __init__
+print(user.name) #check if the Keanu assigned to def __init__
+user.talk()
+
+#-------INHERITANCE-------------
+#example:
+class DogA:
+    def walk(self):
+        print("walk") #this is bad. we have duplicate with Cat class with same purpose
+
+class CatA:
+    def walk(self):
+        print("walk") #this is bad. we have duplicate with Dog class with same purpose
+
+#in inheritance. we can inherit same def/construcotr in different classes
+#HOW TO INHERTIANCE
+
+class Mammal:
+    def walk(self):
+        print("walk")
+class Dog(Mammal):
+    def bark(self):
+        print("woof woof") 
+
+class Cat(Mammal):
+    pass # pass means, hey pass this line, ignore this. we can get error since we have class BUT without its own constructor.
+
+dogs = Dog()
+dogs.walk()
+dogs.bark()
+cats = Cat()
+cats.walk()
